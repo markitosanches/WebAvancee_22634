8db41989002cd5961b69282094cdaa67ff882177
Marcos <markitosanches@gmail.com>
2023-08-22 17:09:08 -0400

    seance 3

---
+++ rest-finance/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const request = require('request')
+const { PORT } = require('./config.js')
+const { API_KEY } = require('./config.js')
+
+app.get('/ticker=:id', function(req, res){
+    //res.end(req.params.id)
+    var ticker = req.params.id
+    const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ticker+'&interval=5min&apikey='+API_KEY;
+
+    request.get({
+        url: url,
+        json: true,
+        headers: {'User-Agent': 'request'}
+      }, (err, res, data) => {
+        if (err) {
+          console.log('Error:', err)
+        } else if (res.statusCode !== 200) {
+          console.log('Status:', res.statusCode)
+        } else {
+          // data is successfully parsed as a JSON object:
+          console.log(data)
+        }
+    })
+    res.send('success')
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT", PORT)
+})
--- rest/index.js
+++ rest/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended:true }))
 app.get('/listUsers', function (req, res) {
     fs.readFile(__dirname + "/user.json", "utf-8", function(err, data){
         if(err) throw err
-        console.log(data)
+       // console.log(data)
         res.end(data)
     })
 })
@@ -34,6 +34,48 @@ app.post('/add-user', function(req, res) {
             console.log('success')
         })
     })
+})
+app.get('/form-data', function(req, res){
+    res.sendFile(__dirname + '/form-data.html');
+})
+
+app.get('/id=:id', function (req, res){
+    fs.readFile(__dirname + "/user.json", "utf-8", function(err, data){
+        if(err) throw err
+        var users = JSON.parse(data)
+        var user = users["user"+req.params.id]
+        console.log(user)
+        res.end(JSON.stringify(user))
+    })
+})
+
+app.put('/update-user', function(req, res){
+    //console.log(req.body)
+    fs.readFile(__dirname + "/user.json", "utf-8", function (err, data){
+        if(err) throw err
+        data = JSON.parse(data)
+        data["user"+req.body.id] = req.body
+        var newData = JSON.stringify(data)
+        fs.writeFile('user.json', newData, err => {
+            if(err) throw err
+            console.log('success')
+        })
+    })
+})
+
+app.delete('/delete-user/:id', function(req, res){
+    //console.log(req.params.id)
+    fs.readFile(__dirname + "/user.json", "utf-8", function (err, data){
+        if(err) throw err
+        data = JSON.parse(data)
+        delete data["user"+req.params.id]
+        var newData = JSON.stringify(data)
+        fs.writeFile('user.json', newData, err => {
+            if(err) throw err
+            console.log('success')
+        })
+    })
+
 })
 
 app.listen(8081, function () {
